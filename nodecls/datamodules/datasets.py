6c46ed08c3f7c6397bf1d69aecd8b3c29b8b67da
Vu Tran <vuptran@gmail.com>
2025-11-24 02:11:58 +0000

    Add NumPyDataset

---
+++ nodecls/datamodules/datasets.py
@@ -0,0 +1,28 @@
+import numpy as np
+from torch.utils.data import Dataset
+
+
+class NumPyDataset(Dataset):
+    def __init__(
+        self,
+        data_path: str,
+        split_type: str,
+    ):
+        assert split_type in ["train", "val", "test"]
+        data = np.load(data_path, allow_pickle=True)
+        self.masks = data[f"{split_type}_masks"]
+        self.feats = data["features"].astype(np.float32)
+        self.labels = data["labels"].astype(np.int64)
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, index):
+        data = {}
+        
+        data["index"] = index
+        data["masks"] = self.masks[index]
+        data["x"] = self.feats[index]
+        data["y"] = self.labels[index]
+        
+        return data
