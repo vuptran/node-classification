f71be7982d77f8ac635cde144e3f959995ae0488
Vu Tran <vuptran@gmail.com>
2025-11-26 03:17:59 +0000

    Update training protocol to use both GNN and MLP models

--- nodecls/train.py
+++ nodecls/train.py
@@ -5,8 +5,9 @@ import numpy as np
 import random
 import torch
 
-from datamodules import GraphDataModule
+from datamodules import NumPyDataModule, GraphDataModule
 from gnn.models import GNNModel
+from mlp.models import MLPModel
 
 from lightning.pytorch.callbacks import ModelCheckpoint
 
@@ -24,6 +25,11 @@ def fix_seed(seed):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model-type",
+        type=str,
+        choices=["gnn", "mlp"],
+        default="gnn")
     parser.add_argument(
         "--layer-name",
         type=str,
@@ -47,29 +53,48 @@ if __name__ == "__main__":
     parser.add_argument("--num-workers", type=int, default=4)
     parser.add_argument("--num-devices", type=int, default=1)
     parser.add_argument("--seed", type=int, default=1108)
+    parser.add_argument("--test-on-gpu", action="store_true")
     parser.add_argument("--test-after-train", action="store_true")
     parser.add_argument("--checkpoint-path", type=str, default=".")
     args = parser.parse_args()
 
     fix_seed(args.seed)
 
-    datamodule = GraphDataModule(
-        data_path=args.data_path,
-        train_batch_size=args.train_batch_size,
-        test_batch_size=args.test_batch_size,
-        num_workers=args.num_workers,
-    )
-    modelmodule = GNNModel(
-        in_channels=datamodule.train_dataset[0]["x"].shape[-1],
-        hidden_channels=args.hidden_channels,
-        out_channels=args.num_classes,
-        layer_name=args.layer_name,
-        num_layers=args.num_layers,
-        in_dropout=args.input_dropout,
-        dropout=args.dropout,
-        learning_rate=args.learning_rate,
-        eval_metric=args.eval_metric,
-    )
+    if args.model_type == "gnn":
+        datamodule = GraphDataModule(
+            data_path=args.data_path,
+            train_batch_size=args.train_batch_size,
+            test_batch_size=args.test_batch_size,
+            num_workers=args.num_workers,
+        )
+        modelmodule = GNNModel(
+            in_channels=datamodule.train_dataset[0]["x"].shape[-1],
+            hidden_channels=args.hidden_channels,
+            out_channels=args.num_classes,
+            layer_name=args.layer_name,
+            num_layers=args.num_layers,
+            in_dropout=args.input_dropout,
+            dropout=args.dropout,
+            learning_rate=args.learning_rate,
+            eval_metric=args.eval_metric,
+        )
+    else:
+        datamodule = NumPyDataModule(
+            data_path=args.data_path,
+            train_batch_size=args.train_batch_size,
+            test_batch_size=args.test_batch_size,
+            num_workers=args.num_workers,
+        )
+        modelmodule = MLPModel(
+            in_channels=datamodule.train_dataset[0]["x"].shape[-1],
+            hidden_channels=args.hidden_channels,
+            out_channels=args.num_classes,
+            num_layers=args.num_layers,
+            in_dropout=args.input_dropout,
+            dropout=args.dropout,
+            learning_rate=args.learning_rate,
+            eval_metric=args.eval_metric,
+        )
 
     # Train and validate.
     best_mode = "min" if args.eval_metric == "mse" else "max"
@@ -98,11 +123,12 @@ if __name__ == "__main__":
     trainer.fit(modelmodule, datamodule=datamodule)
     
     if args.test_after_train:
-        # Test on CPU using the best checkpoint.
+        # Test on a single device using the best validation checkpoint.
+        accelerator = "gpu" if args.test_on_gpu else "cpu"
         best_ckpt_path = trainer.checkpoint_callback.best_model_path
         tester = L.Trainer(
             devices=1,
-            accelerator="cpu",
+            accelerator=accelerator,
             enable_progress_bar=True,
         )
         tester.test(modelmodule, datamodule=datamodule, ckpt_path=best_ckpt_path)
