4c353b3efc1abf8e1b2a9c4dc2f03a5282399954
Vu Tran <vuptran@gmail.com>
2025-11-26 03:12:23 +0000

    Update datamodules to support MLP model

--- nodecls/datamodules/datamodules.py
+++ nodecls/datamodules/datamodules.py
@@ -13,6 +13,70 @@ from torch_geometric.utils import (
 )
 
 
+class NumPyDataModule(L.LightningDataModule):
+    def __init__(
+        self,
+        data_path,
+        train_batch_size=4096,
+        test_batch_size=4096 * 2,
+        num_workers=4,
+    ):
+        super().__init__()
+        self.data_path = data_path
+        self.train_batch_size = train_batch_size
+        self.test_batch_size = test_batch_size
+        self.num_workers = num_workers
+
+        self.train_dataset = NumPyDataset(
+            data_path,
+            split_type="train",
+            apply_masking=True,
+        )
+        self.val_dataset = NumPyDataset(
+            data_path,
+            split_type="val",
+            apply_masking=True,
+        )
+        self.test_dataset = NumPyDataset(
+            data_path,
+            split_type="test",
+            apply_masking=True,
+        )
+    
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.train_batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+            drop_last=True,
+            persistent_workers=True,
+            pin_memory=True,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.test_batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            drop_last=False,
+            persistent_workers=True,
+            pin_memory=True,
+        )
+    
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.test_batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            drop_last=False,
+            persistent_workers=True,
+            pin_memory=True,
+        )
+
+
 class GraphDataModule(L.LightningDataModule):
     def __init__(
         self,
